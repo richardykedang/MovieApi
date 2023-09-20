b31b16be62151b001dd7ae178d49c21ceb689d2b
richardy_pn.p <richardy_pn.p@bi.go.id>
2023-09-20 15:45:11 +0700

    Create DataDummy

--- MovieApi/Data/AppDbContext .cs
+++ MovieApi/Data/AppDbContext .cs
@@ -12,5 +12,14 @@ namespace MovieApi.Data
         }
 
         public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var MovieList = new Movie[]
+            {
+                new Movie { id = 1,title = "Pengabdi Setan 2", description = "Film Horor tahun2022", rating = Convert.ToSingle(7.0), image = "", created_at = DateTime.Now, updated_at = DateTime.Now}
+            };
+            modelBuilder.Entity<Movie>().HasData(MovieList);
+        }
     }
 }
---
+++ MovieApi/Migrations/20230920084320_CreateDummyData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDummyData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "id", "created_at", "description", "image", "rating", "title", "updated_at" },
+                values: new object[] { 1, new DateTime(2023, 9, 20, 15, 43, 20, 115, DateTimeKind.Local).AddTicks(5301), "Film Horor tahun2022", "", 7f, "Pengabdi Setan 2", new DateTime(2023, 9, 20, 15, 43, 20, 115, DateTimeKind.Local).AddTicks(5315) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "id",
+                keyValue: 1);
+        }
+    }
+}
